75966afc50249a4d4887895235dea82a2724c37c
Marko <mdt@wegewerk.com>
2026-07-03 14:13:03 +0000

    prepare release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,19 @@ and this project adheres to [Semantic Versioning](http://semver.org/)
 ## [Unreleased]
 
 
+## [0.9.3] - 2026-07-03
+
+
+## [0.9.3] - 2026-07-03
+
+
+## [0.9.3] - 2026-07-03
+
+
+## [0.9.3] - 2026-07-03
+## CI
+-add github action to automate TER deployment
+
 ## [0.9.2] - 2026-07-03
 ## added
 - quickmeta Tab
---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Ai3 Quickmeta',
+    'description' => 'The metadata Capability of Ai3 Suite. Generate Page Metadata using AI',
+    'category' => 'be',
+    'author' => 'wegewerk developers',
+    'author_email' => 'typo3@wegewerk.com',
+    'author_company' => 'wegewerk GmbH',
+    'state' => 'stable',
+    'version' => '0.9.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-14.3.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
